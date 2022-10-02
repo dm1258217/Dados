486277290d5b4f95dc278a40e6d9e4109806d416
David Michael <michael.david.borges@gmail.com>
2022-10-02 01:33:47 -0300

    Initial commit

---
+++ 127_0_0_1.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.2.0
+ */
+
+/**
+ * Database `cadastro`
+ */
+
+/* `cadastro`.`client` */
+$client = array(
+  array('id_cliente' => '127','nome' => 'David','sobrenome' => 'Michael','cpf' => '42054733890','rg' => '566237222','telefone' => '16981935129','pais' => 'brasil','estado' => 'são paulo','cidade' => 'Franca','bairro' => 'Centro','cep' => '14415000','rua' => 'Dez de març','numero' => '2556','complemento' => 'Casa 2')
+);
+/**
+ * Database `phpmyadmin`
+ */
+
+/* `phpmyadmin`.`pma__bookmark` */
+$pma__bookmark = array(
+);
+
+/* `phpmyadmin`.`pma__central_columns` */
+$pma__central_columns = array(
+);
+
+/* `phpmyadmin`.`pma__column_info` */
+$pma__column_info = array(
+);
+
+/* `phpmyadmin`.`pma__designer_settings` */
+$pma__designer_settings = array(
+);
+
+/* `phpmyadmin`.`pma__export_templates` */
+$pma__export_templates = array(
+);
+
+/* `phpmyadmin`.`pma__favorite` */
+$pma__favorite = array(
+);
+
+/* `phpmyadmin`.`pma__history` */
+$pma__history = array(
+);
+
+/* `phpmyadmin`.`pma__navigationhiding` */
+$pma__navigationhiding = array(
+);
+
+/* `phpmyadmin`.`pma__pdf_pages` */
+$pma__pdf_pages = array(
+);
+
+/* `phpmyadmin`.`pma__recent` */
+$pma__recent = array(
+  array('username' => 'root','tables' => '[{"db":"mysql","table":"db"},{"db":"cadastro","table":"cliente"},{"db":"cadastro","table":"Cliente"}]')
+);
+
+/* `phpmyadmin`.`pma__relation` */
+$pma__relation = array(
+);
+
+/* `phpmyadmin`.`pma__savedsearches` */
+$pma__savedsearches = array(
+);
+
+/* `phpmyadmin`.`pma__table_coords` */
+$pma__table_coords = array(
+);
+
+/* `phpmyadmin`.`pma__table_info` */
+$pma__table_info = array(
+);
+
+/* `phpmyadmin`.`pma__table_uiprefs` */
+$pma__table_uiprefs = array(
+);
+
+/* `phpmyadmin`.`pma__tracking` */
+$pma__tracking = array(
+);
+
+/* `phpmyadmin`.`pma__userconfig` */
+$pma__userconfig = array(
+  array('username' => 'root','timevalue' => '2022-09-30 03:28:31','config_data' => '{"Console\\/Mode":"collapse","lang":"pt"}')
+);
+
+/* `phpmyadmin`.`pma__usergroups` */
+$pma__usergroups = array(
+);
+
+/* `phpmyadmin`.`pma__users` */
+$pma__users = array(
+);
+/**
+ * Database `test`
+ */
